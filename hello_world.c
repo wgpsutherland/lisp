f0ffa3d9954c03bd4f5028fae030e32983faeae8
Will Sutherland <wgpsutherland@gmail.com>
2014-11-06 21:06:13 +0000

    Created prompt - repeats back what you give it

--- hello_world.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char** argv) {
-    puts("Hi, my name is Jarvis. I have just become sentient. Woop.");
-    return 0;
-}
---
+++ prompt.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* If we are compiling on windows then do this */
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+/* Fake readline function */
+char* readline(char* prompt) {
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+    char* cpy = malloc(strlen(buffer)+1);
+    strcpy(cpy, buffer);
+    cpy[strlen(cpy)-1] = '\0';
+    return cpy; 
+}
+
+/* Fake add_history function */
+void add_history(char* unused) {}
+
+/* Otherwise include the editline headers */
+#else
+#include <editline/readline.h>
+#endif
+
+int main(int argc, char** argv) {
+
+    /* Version and exit information for the user */
+    puts("Lispy version 0.0.0.0.1");
+    puts("Press Ctrl+c to exit\n");
+
+    /* This is a loop that never ends */
+    while(1) {
+
+        /* Output the prompt and get input */
+        char* input = readline("lispy> ");
+
+        /* Add input to the history */
+        add_history(input);
+
+        /* Echo input back to the user */
+        printf("No, you're a %s\n", input);
+
+        /* Free retrieved input */
+        free(input);        
+    }
+
+    return 0;
+}
